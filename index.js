468385a4b525038c95c9bc4384130f27a44fbb9a
RaW <akram.iiuc.ctg@gmail.com>
2023-05-02 18:19:24 +0600

    Data file Added

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const cors = require("cors")
 const app = express()
+
+app.use(cors());
 const port = process.env.PORT || 5000
+
+const ChefData = require("./Data/ChefData.json")
 
 app.get('/', (req, res) => {
   res.send('Dragon is running!!')
 })
 
+app.get('/ChefData',(req,res) =>{
+  res.send(ChefData)
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
