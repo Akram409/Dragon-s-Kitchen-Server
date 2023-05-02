6e5383b55694422e734b1d98a2efb3af8889aac3
RaW <akram.iiuc.ctg@gmail.com>
2023-05-03 05:02:28 +0600

    Json data updated and new item added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(cors());
 const port = process.env.PORT || 5000
 
 const ChefData = require("./Data/ChefData.json")
+const recipeData = require("./Data/RecipesData.json")
 
 app.get('/', (req, res) => {
   res.send('Dragon is running!!')
@@ -14,6 +15,18 @@ app.get('/', (req, res) => {
 app.get('/ChefData',(req,res) =>{
   res.send(ChefData)
 })
+
+app.get('/ChefData/:id',(req,res) =>{
+  const id = req.params.id
+  const chef = ChefData.find(pd => pd.id == id)
+  res.send(chef)
+})
+
+app.get('/recipesData',(req,res) =>{
+  res.send(recipeData)
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
